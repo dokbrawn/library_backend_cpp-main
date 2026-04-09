fde313d79905b576f1beb66269de135e69c7e1f2
dokbrawn <tanisimov2006@gmail.com>
2026-04-09 12:31:02 +0700

    test

--- deploy_pi/run.sh
+++ deploy_pi/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LIBRARY_PG_CONN="host=localhost port=5432 dbname=library user=postgres password=123"
+export LIBRARY_PG_CONN="host=10.239.42.46 port=5432 dbname=library user=postgres password=123"
 ./library_flutter_frontend &
 sleep 2
 ./library_backend init
